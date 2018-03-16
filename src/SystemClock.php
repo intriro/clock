f3ee4693ad7e6c163c5baf885517a4aadf530b1b
alsar <ales@intriro.com>
2018-03-16 17:12:03 +0100

    added timezone parameter to SystemClock

--- src/SystemClock.php
+++ src/SystemClock.php
@@ -5,16 +5,27 @@ declare(strict_types=1);
 namespace Intriro\Clock;
 
 use DateTimeImmutable;
+use DateTimeZone;
 
 class SystemClock implements Clock
 {
     use CurrentDateTimeTrait;
 
     /**
+     * @var DateTimeZone
+     */
+    private $timezone;
+
+    public function __construct(DateTimeZone $timezone = null)
+    {
+        $this->timezone = $timezone ?: new DateTimeZone(date_default_timezone_get());
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function now(): DateTimeImmutable
     {
-        return new DateTimeImmutable('now');
+        return new DateTimeImmutable('now', $this->timezone);
     }
 }
